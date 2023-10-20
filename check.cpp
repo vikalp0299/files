1521cf42ab27decd64bd4189ff68563e19adb5f5
vikalp0299 <vparashar0299@gmail.com>
2023-10-20 21:12:36 +0530

    bst

--- check.cpp
+++ check.cpp
@@ -1,6 +1,23 @@
 #include <iostream>
 using namespace std;
 
+class BST{
+    BST *rchild, *lchild;
+    int key;
+    public:
+    BST();
+    BST(int);
+    BST* insert(int);
+    void inorder(BST*);
+};
+
+BST::BST():key(0),rchild(NULL),lchild(NULL){}
+
+BST::BST(int x){
+    key = x;
+    lchild = rchild = NULL;
+}
+
 int main(){
-    cout<<"Hello World";
+    
 }
