64628396c22d6d5d1cbd849c76060b3223f8bba0
vikalp0299 <vparashar0299@gmail.com>
2023-10-20 21:18:50 +0530

    added insert

--- check.cpp
+++ check.cpp
@@ -7,7 +7,7 @@ class BST{
     public:
     BST();
     BST(int);
-    BST* insert(int);
+    BST* insert(BST*,int);
     void inorder(BST*);
 };
 
@@ -16,6 +16,19 @@ BST::BST():key(0),rchild(NULL),lchild(NULL){}
 BST::BST(int x){
     key = x;
     lchild = rchild = NULL;
+}
+BST* BST::insert(BST* root, int key){
+    if(!root){
+        return new BST(key);
+    }
+    if(key > root->key){
+        root->rchild = insert(root->rchild,key);
+    }
+    else if(key < root->key){
+        root->lchild = insert(root->lchild, key);
+    }
+    return root;
+
 }
 
 int main(){
